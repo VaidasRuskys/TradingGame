10acbee43952a253fdfa6e3affe7fb963659ee42
VaidasRuskys <vaidas.ruskys@gmail.com>
2020-09-29 20:00:31 +0300

    Logger added

--- code/src/Controller/ChallengeController.php
+++ code/src/Controller/ChallengeController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Service\Challenge\DayChallengeProvider;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Doctrine\ORM\EntityManager;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
@@ -45,22 +46,26 @@ class ChallengeController extends AbstractController
      * @param Request $request
      * @param DayChallengeProvider $challengeProvider
      * @param EntityManager $entityManager
+     * @param LoggerInterface $logger
      * @return Response
      */
     public function choice(
         Request $request,
         DayChallengeProvider $challengeProvider,
-        EntityManager $entityManager
+        EntityManager $entityManager,
+        LoggerInterface $logger
     ) {
         $user = $this->getUser();
         $action = $request->get('action');
         $dayChallengeId = $request->get('day_challenge_id');
 
         if (!$dayChallengeId) {
+            $logger->error('challenge not found');
             throw new BadRequestException('challenge not found');
         }
 
         if (!$action) {
+            $logger->error('action not found');
             throw new BadRequestException('action not found');
         }
 
@@ -69,6 +74,7 @@ class ChallengeController extends AbstractController
                 $dayChallenge = $challengeProvider->getDayChallenge();
 
                 if ($dayChallengeId != $dayChallenge['id']) {
+                    $logger->error('Challenge not active');
                     throw new \Exception('Challenge not active');
                 }
 
@@ -85,9 +91,12 @@ class ChallengeController extends AbstractController
                 $entityManager->persist($challenge);
                 $entityManager->flush($challenge);
             } catch (UniqueConstraintViolationException $exception) {
+                $logger->error($exception->getMessage());
             } catch (\Exception $exception) {
+                $logger->error($exception->getMessage());
             }
         } else {
+            $logger->error('User not found');
             throw new NotFoundHttpException('User not found');
         }
 
